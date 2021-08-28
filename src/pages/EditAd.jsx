39c356fe4ea093a4611f0f9c11084d2109c70c58
Anusha Kansal <59743854+anushakansal@users.noreply.github.com>
2021-08-29 00:22:23 +0530

    Added alert on request block for edit ad

--- src/pages/EditAd.jsx
+++ src/pages/EditAd.jsx
@@ -8,6 +8,7 @@ export default function EditAd() {
 
   const { id } = useParams();
   const [ad, setAd] = useState(null);
+  const [alert, setAlert] = useState(null);
 
   useEffect(() => {
     const getAd = async (_id) => {
@@ -15,13 +16,25 @@ export default function EditAd() {
         const result = await axios.get(`${BASE_API_URL}/advertisements/${_id}`, { withCredentials: true });
         setAd(result.data);
       } catch(err) {
-        //
+        if(err.request) {
+          setAlert('Please Disable your AdBlocker, it blocked the API request');
+        }
       }
     }
     if(id) {
       getAd(id);
     }
   }, [id]);
+
+  if(alert) {
+    return (
+      <div class="alert-banner my-4">
+        <div class="alert alert-danger" role="alert">
+          {alert}
+        </div>
+      </div>
+    );
+  }
 
   if(ad) {
     return <AdDetailsForm ad={ad}  heading="Edit Your Ad" />;
